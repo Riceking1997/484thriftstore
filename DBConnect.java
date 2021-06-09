173758d50a54e5da94e3f254b75d64531a1b0508
bwishy27 <84643984+bwishy27@users.noreply.github.com>
2021-06-09 16:17:38 -0400

    Add files via upload

---
+++ DBConnect.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import oracle.jdbc.pool.OracleDataSource;
+
+/**
+ *
+ * @author bdubw
+ */
+public class DBConnect extends Application {
+    
+    Connection dbConn;
+    Statement commStmt;
+    ResultSet dbResults;
+    
+    @Override
+    public void start(Stage primaryStage) {
+            
+    }
+    public static void main(String[] args) {
+        launch(args);
+        
+    }
+    public void sendDBCommand(String sqlQuery)
+    {
+        // Set up your connection strings
+        // IF YOU ARE IN CIS330 NOW: use YOUR Oracle Username/Password
+        String URL = "jdbc:oracle:thin:@localhost:1521:XE";
+        String userID = "moneyuser"; // Change to YOUR Oracle username
+        String userPASS = "moneypass"; // Change to YOUR Oracle password
+        OracleDataSource ds;
+        
+        // Clear Box Testing - Print each query to check SQL syntax
+        //  sent to this method.
+        // You can comment this line out when your program is finished
+        System.out.println(sqlQuery);
+        
+        // Lets try to connect
+        try
+        {
+            // instantiate a new data source object
+            ds = new OracleDataSource();
+            // Where is the database located? Web? Local?
+            ds.setURL(URL);
+            // Send the user/pass and get an open connection.
+            dbConn = ds.getConnection(userID,userPASS);
+            // When we get results
+            //  -TYPE_SCROLL_SENSITIVE means if the database data changes we
+            //   will see our resultset update in real time.
+            //  -CONCUR_READ_ONLY means that we cannot accidentally change the
+            //   data in our database by using the .update____() methods of
+            //   the ResultSet class - TableView controls are impacted by
+            //   this setting as well.
+            commStmt = dbConn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            // We send the query to the DB. A ResultSet object is instantiated
+            //  and we are returned a reference to it, that we point to from
+            // dbResults.
+            // Because we declared dbResults at the datafield level
+            // we can see the results from anywhere in our Class.
+            dbResults = commStmt.executeQuery(sqlQuery); // Sends the Query to the DB
+            // The results are stored in a ResultSet structure object
+            // pointed to by the reference variable dbResults
+            // Because we declared this variable globally above, we can use
+            // the results in any method in the class.
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e.toString());
+        }
+    }    
+}
