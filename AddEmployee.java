b711000734da136d33e516197365915b08e156f1
kgutual <80112278+kgutual@users.noreply.github.com>
2021-06-17 20:08:43 -0400

    Update AddEmployee.java

--- AddEmployee.java
+++ AddEmployee.java
@@ -5,6 +5,10 @@
  */
 package Capstone;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashSet;
 import javafx.application.Application;
@@ -16,6 +20,7 @@ import javafx.scene.control.cell.ComboBoxListCell;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import javafx.scene.control.TextArea;
+import oracle.jdbc.pool.OracleDataSource;
 
 
 public class AddEmployee  {
@@ -30,7 +35,7 @@ public class AddEmployee  {
     Label lblEmpName = new Label("Employee Name: ");
     Label lblEmpPhone = new Label("Phone Number: ");
     Label lblEmpAddress = new Label("Address: ");
-    Label lblEmpPeriodSalary = new Label("Salary: ");
+    Label lblEmpPeriodSalary = new Label("Hourly Pay: ");
     Label lblEmpType = new Label("Type: ");
     Label lblEmpStore = new Label("Store: ");
     Label lblEmpUsername = new Label("Username: ");
@@ -41,42 +46,46 @@ public class AddEmployee  {
     TextField txtEmpPhone = new TextField();
     TextField txtEmpAddress = new TextField();
     TextField txtEmpPeriodSalary = new TextField();
-    TextField txtEmpType = new TextField();
-    TextField txtEmpStore = new TextField();
+    ComboBox txtEmpType = new ComboBox();
+    ComboBox txtEmpStore = new ComboBox();
     TextField txtEmpUsername = new TextField();
     TextField txtEmpPassword = new TextField();
     Button btnAddEmployee = new Button("Add ->");
     TextField txtEmpEmail = new TextField();
     ArrayList<Employee> empData = new ArrayList<>();
+    String lastEmpID;
+    int idDigits;
+    String newEmpID;
     
     
     public AddEmployee(ThriftyStoreGUI parentReference){
         mainReference = parentReference;
         primaryPane = new GridPane();
         
-        
+        txtEmpStore.getItems().addAll("sto22221","sto22222","sto22223","sto22224","sto22225","sto22226");
+        txtEmpType.getItems().addAll("cashier","stocker","manager");
         primaryPane.add(lblAdd,0,0);
-        primaryPane.add(lblEmpID,0,1);
-        primaryPane.add(txtEmpID,1,1);
-        primaryPane.add(lblEmpStore,0,2);
-        primaryPane.add(txtEmpStore,1,2);
-        primaryPane.add(lblEmpName,0,3);
-        primaryPane.add(txtEmpName,1,3);
-        primaryPane.add(lblEmpAddress,0,4);
-        primaryPane.add(txtEmpAddress,1,4);
-        primaryPane.add(lblEmpPhone,0,5);
-        primaryPane.add(txtEmpPhone,1,5);
-        primaryPane.add(lblEmpEmail, 0, 6);
-        primaryPane.add(txtEmpEmail, 1, 6);
-        primaryPane.add(lblEmpPeriodSalary,0,7);
-        primaryPane.add(txtEmpPeriodSalary,1,7);
-        primaryPane.add(lblEmpUsername,0,8);
-        primaryPane.add(lblEmpPassword,0,9);
-        primaryPane.add(txtEmpUsername,1,8);
-        primaryPane.add(txtEmpPassword,1,9);
-        primaryPane.add(lblEmpType,0,10);
-        primaryPane.add(txtEmpType,1,10);
-        primaryPane.add(btnAddEmployee,1,11);
+//        primaryPane.add(lblEmpID,0,1);
+//        primaryPane.add(txtEmpID,1,1);
+        primaryPane.add(lblEmpStore,0,1);
+        primaryPane.add(txtEmpStore,1,1);
+        primaryPane.add(lblEmpName,0,2);
+        primaryPane.add(txtEmpName,1,2);
+        primaryPane.add(lblEmpAddress,0,3);
+        primaryPane.add(txtEmpAddress,1,3);
+        primaryPane.add(lblEmpPhone,0,4);
+        primaryPane.add(txtEmpPhone,1,4);
+        primaryPane.add(lblEmpEmail, 0, 5);
+        primaryPane.add(txtEmpEmail, 1, 5);
+        primaryPane.add(lblEmpPeriodSalary,0,6);
+        primaryPane.add(txtEmpPeriodSalary,1,6);
+        primaryPane.add(lblEmpUsername,0,7);
+        primaryPane.add(lblEmpPassword,0,8);
+        primaryPane.add(txtEmpUsername,1,7);
+        primaryPane.add(txtEmpPassword,1,8);
+        primaryPane.add(lblEmpType,0,9);
+        primaryPane.add(txtEmpType,1,9);
+        primaryPane.add(btnAddEmployee,1,10);
         
         primaryPane.setAlignment(Pos.CENTER);
         
@@ -86,16 +95,19 @@ public class AddEmployee  {
         primaryStage.setScene(primaryScene);
         primaryStage.setTitle("Add New Employee");
         primaryStage.show();
-        
-        
-        
+        lastEmpID = mainReference.employeeID().getEmployeeID();
+        idDigits = Integer.parseInt(lastEmpID.substring(3));
+        idDigits++;
+        newEmpID = "emp" + idDigits;
+        System.out.println(newEmpID);
         
         
         btnAddEmployee.setOnAction(e -> {
-            empData.add(new Employee(txtEmpID.getText(),txtEmpStore.getText(),txtEmpName.getText(),txtEmpAddress.getText(),
+            empData.add(new Employee(newEmpID,txtEmpStore.getSelectionModel().getSelectedItem().toString(),txtEmpName.getText(),txtEmpAddress.getText(),
                                  txtEmpPhone.getText(), txtEmpEmail.getText(), Double.valueOf(txtEmpPeriodSalary.getText()), txtEmpUsername.getText(),
-                                 txtEmpPassword.getText(), txtEmpType.getText()));
+                                 txtEmpPassword.getText(), txtEmpType.getSelectionModel().getSelectedItem().toString()));
             mainReference.AddEmployee(empData.get(0));
+            
 //            txtEmpID.clear();
 //            txtEmpStore.clear();
 //            txtEmpName.clear();
@@ -107,7 +119,27 @@ public class AddEmployee  {
 //            txtEmpPassword.clear();
 //            txtEmpType.clear();
 
-            //Add Notification
+            //*******Add Success Notification*****
+            
+            
+            String sql = "INSERT INTO JAVAUSER.EMPLOYEE (EMPLOYEEID,STOREID,NAME,ADDRESS,PHONE,EMAIL,PERIODSALARY,USERNAME,PASSWORD,HOURLYPAY,TYPE) VALUES ('";
+            sql += newEmpID + "', '";
+                    sql += txtEmpStore.getSelectionModel().getSelectedItem().toString() + "', '";
+                    sql += txtEmpName.getText() + "', '"+ txtEmpAddress.getText()+ "', '"+ txtEmpPhone.getText();
+                    sql += "', '"+ txtEmpEmail.getText()+ "', null, '" + txtEmpUsername.getText()+ "', '";
+                    sql += txtEmpPassword.getText()+ "', '"+ txtEmpPeriodSalary.getText();
+                    sql += "', '" + txtEmpType.getSelectionModel().getSelectedItem().toString() + "')";
+            
+//            mainReference.sendDBCommand("INSERT INTO EMPLOYEE  VALUES ('" + newEmpID + "', '"
+//                    + txtEmpStore.getSelectionModel().getSelectedItem().toString() + "', '"
+//                    + txtEmpName.getText() + "', '"+ txtEmpAddress.getText()+ "', '"+ txtEmpPhone.getText() 
+//                    + "', '"+ txtEmpEmail.getText()+ "', null, '" + txtEmpUsername.getText()+ "', '"
+//                    + txtEmpPassword.getText()+ "', '"+ Double.valueOf(txtEmpPeriodSalary.getText()) 
+//                    + "', '" + txtEmpType.getSelectionModel().getSelectedItem().toString() + "')");
+            
+            System.out.println(sql);
+            
+            mainReference.sendDBCommand(sql);
             
             primaryStage.close();
             
@@ -118,5 +150,11 @@ public class AddEmployee  {
         
         
     }
+    
+//    public String getSql()
+//    {
+//        return sql;
+//    }
+
     
 }
