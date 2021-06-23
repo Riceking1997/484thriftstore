dd5f987eb689a8afebc0abc779bb59514f64eb99
alpdiallo <84424101+alpdiallo@users.noreply.github.com>
2021-06-23 12:46:49 -0400

    Update Receipt.java

--- Receipt.java
+++ Receipt.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Capstone;
+package Thifty2;
 
 import java.util.ArrayList;
 
@@ -94,5 +94,9 @@ public class Receipt {
     
     public void setDate(String time){
         this.date = time;
+    }
+    
+    public void setItemList(ArrayList<ReceiptItem> List) {
+        this.itemList = List;
     }
 }
